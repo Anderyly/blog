b32ac700a94e7b5b8fe6065bb8ac714ef90d014e
anderyly <admin@aaayun.cc>
2024-06-11 18:53:54 +0800

    g
    Signed-off-by: anderyly <admin@aaayun.cc>

--- routers/admin.go
+++ routers/admin.go
@@ -8,13 +8,12 @@
 package routers
 
 import (
-	"blog/app/controllers/admin"
 	"github.com/gin-gonic/gin"
 )
 
 func AdminRouter(r *gin.Engine) {
 
-	router := r.Group("/admin")
-	router.GET("login", admin.LoginController{}.Login)
-	router.POST("login", admin.LoginController{}.Login)
+	//router := r.Group("/admin")
+	//router.GET("login", admin.LoginController{}.Login)
+	//router.POST("login", admin.LoginController{}.Login)
 }
